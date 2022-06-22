278a85d07396ca46bdddc3417b7172d1087c2fb0
N-Hidaya <nurul-h@hotmail.com>
2022-06-22 20:53:27 +0800

    lesson 8 added

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const content = `
                     <li>Number of pockets: ${everydayPack.pocketNum}</li>
                     <li>Left strap length: ${everydayPack.strapLengthL}</li>
                     <li>Right strap length: ${everydayPack.strapLengthR}</li>
-                    <li>Lid status: ${everydayPack.lidOpen}</li>
+                    <li>Lid status: ${everydayPack.lidOpen ? "open" : "closed"}</li>
                 </ul>
         
 `;
@@ -83,6 +83,108 @@ newArticle.setAttribute("id", "everyday");
 newArticle.innerHTML = content;
 
 main.append(newArticle);
+
+if (everydayPack.lidOpen === true) {
+    console.log("Lid is open");
+} else {
+    console.log("Lid is closed");
+}
+
+console.log(everydayPack.lidOpen ? "open" : "closed");
+
+//switch statements
+const usedStatus = () => {
+    let age = everydayPack.age;
+    age = 100;
+    let description;
+
+    switch(true) {
+        case age < 30:
+            description = "new";
+            break;
+        case age >= 30 && age < 365:
+            description = "lightly used";
+            break;
+        case age >= 365 && age < 1095:
+            description = "used";
+            break;
+        case age >= 1095:
+            description ="old";
+            break;
+        default:
+            console.log("There is no description")
+    }
+
+    console.log(`
+        Age: ${age} days
+        Status: ${description}
+    `);
+};
+
+usedStatus();
+
+const stuff = ["piggy", "headlamp", "pen", "pencil", "eraser"];
+//Looping through content
+const article = document.querySelector("article");
+let stuffList = document.createElement("ul");
+
+for (let i=0; i<stuff.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = stuff[i];
+    stuffList.append(listItem);
+}
+
+//another modern JS method
+for (const item of stuff) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = item;
+    stuffList.append(listItem);
+}
+
+//foreach array method
+stuff.forEach((item) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = item;
+    stuffList.append(listItem);
+});
+
+//map() array method
+const stuffItems = stuff.map((item) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = item;
+    return listItem;
+});
+
+stuffItems.forEach((item) => {
+    stuffList.append(item);
+});
+
+console.log("stuff: ", stuff);
+console.log("stuffItem: ", stuffItems)
+
+//for...in loop and objects
+for (const singleObject in nestedObjects) {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `Name: ${nestedObjects[singleObject].name}`
+    stuffList.append(listItem);
+}
+
+const addPack = (currentPack) => {
+    const newArticle = document.createElement("article");
+    newArticle.innerHTML = `
+        <h1>${currentPack.name}</h1>
+        <ul>
+            <li>Volume: ${currentPack.volume}</li>
+            <li>Color: ${currentPack.color}</li>
+            <li>Number of pockets: ${currentPack.pocketNum}</li>
+        </ul>
+    `;
+    return newArticle;
+}
+
+const theArticle = addPack(backpack);
+console.log(theArticle);
+main.append(addPack(backpack));
 
 //Add a navigation section to the DOM
 const navContent = `
@@ -168,3 +270,97 @@ numbers.push(6); // [1,2,3,4,5,6]
 numbers.unshift(7); // [7,1,2,3,4,5,6]
 numbers.pop(); // [7,1,2,3,4,5]
 numbers.shift(); // [1,2,3,4,5]
+
+//Function declaration
+function doSomeMath(a, b) {
+    let c = a + b
+    return c;
+}
+
+//Function expression
+const doMoreMath = function (a, b)  {
+    let c = a*b;
+    return c;
+};
+
+console.log(doSomeMath(5,6));
+console.log(doMoreMath(5,6));
+
+//Immediately invoked function expression (IIFE)
+(function () {
+    let a = 4;
+    let b = 6;
+    let c = doSomeMath(a, b);
+    console.log(`The sum of a and b is ${c}`);
+})();
+
+//Arrow function with few parameters
+const tipCalculator = (sum, percentage, currency, prefix) => {
+    let tip = sum * (percentage / 100);
+    let total = sum + tip;
+    if(prefix){
+        console.log(`
+            Sum before tip: ${currency}${sum}
+            Tip percentage: ${percentage}%
+            Tip:            ${currency}${tip.toFixed(2)}
+            Total:          ${currency}${total.toFixed(2)}
+        `);
+    } else {
+        console.log(`
+            Sum before tip: ${sum}${currency}
+            Tip percentage: ${percentage}%
+            Tip:            ${tip.toFixed(2)}${currency}
+            Total:          ${total.toFixed(2)}${currency}
+        `);
+    }
+};
+
+tipCalculator(29.95, 28, "$", true);
+
+//Using country currencies formatter
+const formatter = (locale, currency, value) => {
+    let formattedValue = new Intl.NumberFormat(locale, {
+        style: "currency",
+        currency: currency,
+
+    }).format(value);
+    return formattedValue;
+};
+
+const finalCalculator = (locale, currency, sum, percentage, callback) => {
+    let tip = sum * (percentage / 100);
+    let total = sum + tip;
+    const finalTip = {
+        sum: formatter(locale, currency, sum),
+        percentage: percentage + "%",
+        tip: formatter(locale, currency, tip),
+        total: formatter(local, currency, total)
+    };
+    callback(finalTip);
+};
+
+finalCalculator("de-DE", "EUR", 29.95, 18, printCalculator);
+
+//creating HTML interface with callback
+const printCalculator = (finalTip) => {
+    const tipTable = document.createElement("table");
+    tipTable.innerHTML = `
+        <tr>
+            <td>Sum before tip</td>
+            <td>${finalTip.sum}</td>
+        </tr>
+        <tr>
+            <td>Tip percentage</td>
+            <td>$finalTip.percentage</td>
+        </tr>
+        <tr>
+            <td>Tip:</td>
+            <td>${finalTip.tip}</td>
+        </tr>
+        <tr>
+            <td>Total:</td>
+            <td>${finalTip.total}</td>
+        </tr>
+    `;
+    document.querySelector("main").append(tipTable);
+};
